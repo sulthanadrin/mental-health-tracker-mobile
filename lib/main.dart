77100e1520c8fc9a24fadcdaea0f15640f079808
Sulthan Adrin <sulthanadrin0806@gmail.com>
2024-11-06 13:24:27 +0700

    menyelesaikan tutorial 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/menu.dart';
+import 'package:mental_health_tracker/screens/menu.dart';
+
 
 void main() {
   runApp(const MyApp());
